9f353c6e00c837beb017d643ede6ab5c0b39cc78
{马立杰}({060863}) <lijie.ma@alibaba-inc.com>
2015-09-05 13:17:34 +0800

    add region ap-southeast-1

--- aliyun-php-sdk-core/Regions/EndpointConfig.php
+++ aliyun-php-sdk-core/Regions/EndpointConfig.php
@@ -17,7 +17,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-$regionIds = array("cn-hangzhou","cn-beijing","cn-qingdao","cn-hongkong","cn-shanghai","us-west-1","cn-shenzhen");
+$regionIds = array("cn-hangzhou","cn-beijing","cn-qingdao","cn-hongkong","cn-shanghai","us-west-1","cn-shenzhen","ap-southeast-1");
 $productDomains =array(
 	new ProductDomain("Ecs", "ecs.aliyuncs.com"),
 	new ProductDomain("Rds", "rds.aliyuncs.com"),
