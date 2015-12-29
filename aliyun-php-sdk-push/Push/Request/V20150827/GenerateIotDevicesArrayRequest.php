8d1f31f948ce68c4a358c79a404d76ab9bf50b27
hi.yan.li <hi.yan.li@alibaba-inc.com>
2015-12-29 15:03:30 +0800

    add push sdk

---
+++ aliyun-php-sdk-push/Push/Request/V20150827/GenerateIotDevicesArrayRequest.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace Push\Request\V20150827;
+
+class GenerateIotDevicesArrayRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("Push", "2015-08-27", "GenerateIotDevicesArray");
+	}
+
+	private  $isDev;
+
+	private  $number;
+
+	public function getisDev() {
+		return $this->isDev;
+	}
+
+	public function setisDev($isDev) {
+		$this->isDev = $isDev;
+		$this->queryParameters["isDev"]=$isDev;
+	}
+
+	public function getNumber() {
+		return $this->number;
+	}
+
+	public function setNumber($number) {
+		$this->number = $number;
+		$this->queryParameters["Number"]=$number;
+	}
+	
+}
---
+++ aliyun-php-sdk-push/Push/Request/V20150827/PushMessageToiOSRequest.php
@@ -0,0 +1,84 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace Push\Request\V20150827;
+
+class PushMessageToiOSRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("Push", "2015-08-27", "PushMessageToiOS");
+	}
+
+	private  $appKey;
+
+	private  $target;
+
+	private  $targetValue;
+
+	private  $message;
+
+	private  $summary;
+
+	public function getAppKey() {
+		return $this->appKey;
+	}
+
+	public function setAppKey($appKey) {
+		$this->appKey = $appKey;
+		$this->queryParameters["AppKey"]=$appKey;
+	}
+
+	public function getTarget() {
+		return $this->target;
+	}
+
+	public function setTarget($target) {
+		$this->target = $target;
+		$this->queryParameters["Target"]=$target;
+	}
+
+	public function getTargetValue() {
+		return $this->targetValue;
+	}
+
+	public function setTargetValue($targetValue) {
+		$this->targetValue = $targetValue;
+		$this->queryParameters["TargetValue"]=$targetValue;
+	}
+
+	public function getMessage() {
+		return $this->message;
+	}
+
+	public function setMessage($message) {
+		$this->message = $message;
+		$this->queryParameters["Message"]=$message;
+	}
+
+	public function getSummary() {
+		return $this->summary;
+	}
+
+	public function setSummary($summary) {
+		$this->summary = $summary;
+		$this->queryParameters["Summary"]=$summary;
+	}
+	
+}
