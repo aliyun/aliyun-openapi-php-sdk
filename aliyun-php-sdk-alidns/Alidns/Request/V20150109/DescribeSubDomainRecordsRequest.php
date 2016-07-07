b011e67c7ee34b98719112ca27980c6a9324511d
ling.wu <ling.wu@alibaba-inc.com>
2016-07-07 17:21:30 +0800

    add alidns sdk

---
+++ aliyun-php-sdk-alidns/Alidns/Request/V20150109/DescribeSubDomainRecordsRequest.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace Alidns\Request\V20150109;
+
+class DescribeSubDomainRecordsRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("Alidns", "2015-01-09", "DescribeSubDomainRecords");
+	}
+
+	private  $subDomain;
+
+	private  $pageNumber;
+
+	private  $pageSize;
+
+	private  $type;
+
+	public function getSubDomain() {
+		return $this->subDomain;
+	}
+
+	public function setSubDomain($subDomain) {
+		$this->subDomain = $subDomain;
+		$this->queryParameters["SubDomain"]=$subDomain;
+	}
+
+	public function getPageNumber() {
+		return $this->pageNumber;
+	}
+
+	public function setPageNumber($pageNumber) {
+		$this->pageNumber = $pageNumber;
+		$this->queryParameters["PageNumber"]=$pageNumber;
+	}
+
+	public function getPageSize() {
+		return $this->pageSize;
+	}
+
+	public function setPageSize($pageSize) {
+		$this->pageSize = $pageSize;
+		$this->queryParameters["PageSize"]=$pageSize;
+	}
+
+	public function getType() {
+		return $this->type;
+	}
+
+	public function setType($type) {
+		$this->type = $type;
+		$this->queryParameters["Type"]=$type;
+	}
+	
+}
---
+++ aliyun-php-sdk-alidns/Alidns/Request/V20150109/UpdateDomainRecordRequest.php
@@ -0,0 +1,106 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace Alidns\Request\V20150109;
+
+class UpdateDomainRecordRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("Alidns", "2015-01-09", "UpdateDomainRecord");
+	}
+
+	private  $recordId;
+
+	private  $rR;
+
+	private  $type;
+
+	private  $value;
+
+	private  $tTL;
+
+	private  $priority;
+
+	private  $line;
+
+	public function getRecordId() {
+		return $this->recordId;
+	}
+
+	public function setRecordId($recordId) {
+		$this->recordId = $recordId;
+		$this->queryParameters["RecordId"]=$recordId;
+	}
+
+	public function getRR() {
+		return $this->rR;
+	}
+
+	public function setRR($rR) {
+		$this->rR = $rR;
+		$this->queryParameters["RR"]=$rR;
+	}
+
+	public function getType() {
+		return $this->type;
+	}
+
+	public function setType($type) {
+		$this->type = $type;
+		$this->queryParameters["Type"]=$type;
+	}
+
+	public function getValue() {
+		return $this->value;
+	}
+
+	public function setValue($value) {
+		$this->value = $value;
+		$this->queryParameters["Value"]=$value;
+	}
+
+	public function getTTL() {
+		return $this->tTL;
+	}
+
+	public function setTTL($tTL) {
+		$this->tTL = $tTL;
+		$this->queryParameters["TTL"]=$tTL;
+	}
+
+	public function getPriority() {
+		return $this->priority;
+	}
+
+	public function setPriority($priority) {
+		$this->priority = $priority;
+		$this->queryParameters["Priority"]=$priority;
+	}
+
+	public function getLine() {
+		return $this->line;
+	}
+
+	public function setLine($line) {
+		$this->line = $line;
+		$this->queryParameters["Line"]=$line;
+	}
+	
+}
