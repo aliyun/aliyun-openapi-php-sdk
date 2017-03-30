968f8f0c8895abc0f233cea39d55d0dd6e997a97
归邪 <zhangw@alibaba-inc.com>
2017-03-30 15:21:20 +0800

    add sdk vod

--- aliyun-php-sdk-core/Exception/ServerException.php
+++ aliyun-php-sdk-core/Exception/ServerException.php
@@ -19,11 +19,27 @@
  */
 class ServerException extends ClientException
 {
+    private $httpStatus;
+    private $requestId;
 
-	function  __construct($errorMessage, $errorCode)
-	{
-		parent::__construct($errorMessage, $errorCode);
-		$this->setErrorType("Server");
-	}
+    function  __construct($errorMessage, $errorCode, $httpStatus, $requestId)
+    {
+        $messageStr = $errorCode . " " . $errorMessage . " HTTP Status: " . $httpStatus . " RequestID: " . $requestId;
+        parent::__construct($messageStr, $errorCode);
+        $this->setErrorMessage($errorMessage);
+        $this->setErrorType("Server");
+        $this->httpStatus = $httpStatus;
+        $this->requestId = $requestId;
+    }
+
+    public function getHttpStatus()
+    {
+        return $this->httpStatus;
+    }
+
+    public function getRequestId()
+    {
+        return $this->requestId;
+    }
 
 }
