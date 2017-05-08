2a35865b468194ea15b0d4fe5a3e784e6b26bd8c
ling.wu <ling.wu@alibaba-inc.com>
2017-05-08 14:27:25 +0800

    domain sdk 1、增加批量任务-whois保护功能 2、增加批量任务列表查询功能 3、增加批量任务详情查看接口

---
+++ aliyun-php-sdk-domain/Domain/Request/V20160511/QueryBatchTaskListRequest.php
@@ -0,0 +1,95 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace Domain\Request\V20160511;
+
+class QueryBatchTaskListRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("Domain", "2016-05-11", "QueryBatchTaskList");
+	}
+
+	private  $userClientIp;
+
+	private  $lang;
+
+	private  $beginCreateTime;
+
+	private  $endCreateTime;
+
+	private  $pageNum;
+
+	private  $pageSize;
+
+	public function getUserClientIp() {
+		return $this->userClientIp;
+	}
+
+	public function setUserClientIp($userClientIp) {
+		$this->userClientIp = $userClientIp;
+		$this->queryParameters["UserClientIp"]=$userClientIp;
+	}
+
+	public function getLang() {
+		return $this->lang;
+	}
+
+	public function setLang($lang) {
+		$this->lang = $lang;
+		$this->queryParameters["Lang"]=$lang;
+	}
+
+	public function getBeginCreateTime() {
+		return $this->beginCreateTime;
+	}
+
+	public function setBeginCreateTime($beginCreateTime) {
+		$this->beginCreateTime = $beginCreateTime;
+		$this->queryParameters["BeginCreateTime"]=$beginCreateTime;
+	}
+
+	public function getEndCreateTime() {
+		return $this->endCreateTime;
+	}
+
+	public function setEndCreateTime($endCreateTime) {
+		$this->endCreateTime = $endCreateTime;
+		$this->queryParameters["EndCreateTime"]=$endCreateTime;
+	}
+
+	public function getPageNum() {
+		return $this->pageNum;
+	}
+
+	public function setPageNum($pageNum) {
+		$this->pageNum = $pageNum;
+		$this->queryParameters["PageNum"]=$pageNum;
+	}
+
+	public function getPageSize() {
+		return $this->pageSize;
+	}
+
+	public function setPageSize($pageSize) {
+		$this->pageSize = $pageSize;
+		$this->queryParameters["PageSize"]=$pageSize;
+	}
+	
+}
