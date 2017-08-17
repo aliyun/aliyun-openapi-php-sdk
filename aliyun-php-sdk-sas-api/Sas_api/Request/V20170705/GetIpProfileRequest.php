165c200589f3aeac44274b9867a91c7bda45347f
haowei.yao <haowei.yao@alibaba-inc.com>
2017-08-17 15:27:26 +0800

    由顾怀发起的SAS-API SDK自动发布, BUILD_ID=158, 版本号：2.0.1 发布日志： 1, 首次发布态势感知对外sdk 2, 包含情报api两个接口（查询ip情报，查询手机号情报），和售卖的5个接口（查询实例数，新购，续费，升级，退款） 3, 由于之前已把jar包线下提供给客户，为了与当前保持一致，版本号从2.0.1开始

---
+++ aliyun-php-sdk-sas-api/Sas_api/Request/V20170705/GetIpProfileRequest.php
@@ -0,0 +1,140 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace Sas_api\Request\V20170705;
+
+class GetIpProfileRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("Sas-api", "2017-07-05", "GetIpProfile");
+		$this->setMethod("POST");
+	}
+
+	private  $deviceIdMd5;
+
+	private  $carrier;
+
+	private  $os;
+
+	private  $requestUrl;
+
+	private  $ip;
+
+	private  $userAgent;
+
+	private  $connectionType;
+
+	private  $sensType;
+
+	private  $deviceType;
+
+	private  $businessType;
+
+	public function getDeviceIdMd5() {
+		return $this->deviceIdMd5;
+	}
+
+	public function setDeviceIdMd5($deviceIdMd5) {
+		$this->deviceIdMd5 = $deviceIdMd5;
+		$this->queryParameters["DeviceIdMd5"]=$deviceIdMd5;
+	}
+
+	public function getCarrier() {
+		return $this->carrier;
+	}
+
+	public function setCarrier($carrier) {
+		$this->carrier = $carrier;
+		$this->queryParameters["Carrier"]=$carrier;
+	}
+
+	public function getOs() {
+		return $this->os;
+	}
+
+	public function setOs($os) {
+		$this->os = $os;
+		$this->queryParameters["Os"]=$os;
+	}
+
+	public function getRequestUrl() {
+		return $this->requestUrl;
+	}
+
+	public function setRequestUrl($requestUrl) {
+		$this->requestUrl = $requestUrl;
+		$this->queryParameters["RequestUrl"]=$requestUrl;
+	}
+
+	public function getIp() {
+		return $this->ip;
+	}
+
+	public function setIp($ip) {
+		$this->ip = $ip;
+		$this->queryParameters["Ip"]=$ip;
+	}
+
+	public function getUserAgent() {
+		return $this->userAgent;
+	}
+
+	public function setUserAgent($userAgent) {
+		$this->userAgent = $userAgent;
+		$this->queryParameters["UserAgent"]=$userAgent;
+	}
+
+	public function getConnectionType() {
+		return $this->connectionType;
+	}
+
+	public function setConnectionType($connectionType) {
+		$this->connectionType = $connectionType;
+		$this->queryParameters["ConnectionType"]=$connectionType;
+	}
+
+	public function getSensType() {
+		return $this->sensType;
+	}
+
+	public function setSensType($sensType) {
+		$this->sensType = $sensType;
+		$this->queryParameters["SensType"]=$sensType;
+	}
+
+	public function getDeviceType() {
+		return $this->deviceType;
+	}
+
+	public function setDeviceType($deviceType) {
+		$this->deviceType = $deviceType;
+		$this->queryParameters["DeviceType"]=$deviceType;
+	}
+
+	public function getBusinessType() {
+		return $this->businessType;
+	}
+
+	public function setBusinessType($businessType) {
+		$this->businessType = $businessType;
+		$this->queryParameters["BusinessType"]=$businessType;
+	}
+	
+}
---
+++ aliyun-php-sdk-sas-api/Sas_api/Request/V20170705/GetPhoneProfileRequest.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace Sas_api\Request\V20170705;
+
+class GetPhoneProfileRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("Sas-api", "2017-07-05", "GetPhoneProfile");
+		$this->setMethod("POST");
+	}
+
+	private  $phone;
+
+	private  $sensType;
+
+	private  $dataVersion;
+
+	private  $businessType;
+
+	public function getPhone() {
+		return $this->phone;
+	}
+
+	public function setPhone($phone) {
+		$this->phone = $phone;
+		$this->queryParameters["Phone"]=$phone;
+	}
+
+	public function getSensType() {
+		return $this->sensType;
+	}
+
+	public function setSensType($sensType) {
+		$this->sensType = $sensType;
+		$this->queryParameters["SensType"]=$sensType;
+	}
+
+	public function getDataVersion() {
+		return $this->dataVersion;
+	}
+
+	public function setDataVersion($dataVersion) {
+		$this->dataVersion = $dataVersion;
+		$this->queryParameters["DataVersion"]=$dataVersion;
+	}
+
+	public function getBusinessType() {
+		return $this->businessType;
+	}
+
+	public function setBusinessType($businessType) {
+		$this->businessType = $businessType;
+		$this->queryParameters["BusinessType"]=$businessType;
+	}
+	
+}
