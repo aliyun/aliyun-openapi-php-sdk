23bff7b69e69f9dcd9ada739d4892e9cc871d524
haowei.yao <haowei.yao@alibaba-inc.com>
2017-09-07 15:31:07 +0800

    由廷诚发起的CLOUDPHOTO SDK自动发布, BUILD_ID=181, 版本号：1.1.3 发布日志： 1, 新增getDownloadUrls 2, 新增getThumbnails 3, CreatePhoto/GetPhotos/ListPhotos/SearchPhotos新增返回字段TakenAt 4, 修正Cover为null时remark为空字符串的bug 5, 物理删除照片接口增加调用文件服务 deleteFiles 6, 修复特殊情况下重复提交创建多个文件的bug

---
+++ aliyun-php-sdk-cloudphoto/CloudPhoto/Request/V20170711/GetDownloadUrlsRequest.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace CloudPhoto\Request\V20170711;
+
+class GetDownloadUrlsRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("CloudPhoto", "2017-07-11", "GetDownloadUrls", "cloudphoto", "openAPI");
+		$this->setProtocol("https");
+		$this->setMethod("POST");
+	}
+
+	private  $PhotoIds;
+
+	private  $storeName;
+
+	public function getPhotoIds() {
+		return $this->PhotoIds;
+	}
+
+	public function setPhotoIds($PhotoIds) {
+		$this->PhotoIds = $PhotoIds;
+		for ($i = 0; $i < count($PhotoIds); $i ++) {	
+			$this->queryParameters["PhotoId.".($i+1)] = $PhotoIds[$i];
+		}
+	}
+
+	public function getStoreName() {
+		return $this->storeName;
+	}
+
+	public function setStoreName($storeName) {
+		$this->storeName = $storeName;
+		$this->queryParameters["StoreName"]=$storeName;
+	}
+	
+}
---
+++ aliyun-php-sdk-cloudphoto/CloudPhoto/Request/V20170711/GetThumbnailsRequest.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace CloudPhoto\Request\V20170711;
+
+class GetThumbnailsRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("CloudPhoto", "2017-07-11", "GetThumbnails", "cloudphoto", "openAPI");
+		$this->setProtocol("https");
+		$this->setMethod("POST");
+	}
+
+	private  $PhotoIds;
+
+	private  $storeName;
+
+	private  $zoomType;
+
+	public function getPhotoIds() {
+		return $this->PhotoIds;
+	}
+
+	public function setPhotoIds($PhotoIds) {
+		$this->PhotoIds = $PhotoIds;
+		for ($i = 0; $i < count($PhotoIds); $i ++) {	
+			$this->queryParameters["PhotoId.".($i+1)] = $PhotoIds[$i];
+		}
+	}
+
+	public function getStoreName() {
+		return $this->storeName;
+	}
+
+	public function setStoreName($storeName) {
+		$this->storeName = $storeName;
+		$this->queryParameters["StoreName"]=$storeName;
+	}
+
+	public function getZoomType() {
+		return $this->zoomType;
+	}
+
+	public function setZoomType($zoomType) {
+		$this->zoomType = $zoomType;
+		$this->queryParameters["ZoomType"]=$zoomType;
+	}
+	
+}
