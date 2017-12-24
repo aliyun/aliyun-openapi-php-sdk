b5ac7ef30d98972499b1a8b8c56579311274d469
haowei.yao <haowei.yao@alibaba-inc.com>
2017-12-24 20:13:53 +0800

    由荣旸发起的TESLAMAXCOMPUTE SDK自动发布, BUILD_ID=223, 版本号：1.0.3 发布日志： 1, Add get instance by cluster, project and user.

---
+++ aliyun-php-sdk-teslamaxcompute/TeslaMaxCompute/Request/V20171130/GetProjectInstanceRequest.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace TeslaMaxCompute\Request\V20171130;
+
+class GetProjectInstanceRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("TeslaMaxCompute", "2017-11-30", "GetProjectInstance");
+	}
+
+	private  $pageSize;
+
+	private  $project;
+
+	private  $pageNum;
+
+	private  $status;
+
+	public function getPageSize() {
+		return $this->pageSize;
+	}
+
+	public function setPageSize($pageSize) {
+		$this->pageSize = $pageSize;
+		$this->queryParameters["PageSize"]=$pageSize;
+	}
+
+	public function getProject() {
+		return $this->project;
+	}
+
+	public function setProject($project) {
+		$this->project = $project;
+		$this->queryParameters["Project"]=$project;
+	}
+
+	public function getPageNum() {
+		return $this->pageNum;
+	}
+
+	public function setPageNum($pageNum) {
+		$this->pageNum = $pageNum;
+		$this->queryParameters["PageNum"]=$pageNum;
+	}
+
+	public function getStatus() {
+		return $this->status;
+	}
+
+	public function setStatus($status) {
+		$this->status = $status;
+		$this->queryParameters["Status"]=$status;
+	}
+	
+}
