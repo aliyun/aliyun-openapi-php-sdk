315fa0f5dae2bcf8d6bfdb02c1a8e564ef920018
haowei.yao <haowei.yao@alibaba-inc.com>
2017-12-29 00:38:47 +0800

    由择仁发起的CCC SDK自动发布, BUILD_ID=316, 版本号：1.0.1 发布日志： 1, This is an example of release-log. 2, Please strictly follow this format to edit in English. 3, Format：Number + , + Space + Description

---
+++ aliyun-php-sdk-ccc/CCC/Request/V20170705/DownloadRecordingRequest.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace CCC\Request\V20170705;
+
+class DownloadRecordingRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("CCC", "2017-07-05", "DownloadRecording", "ccc", "openAPI");
+		$this->setMethod("POST");
+	}
+
+	private  $instanceId;
+
+	private  $fileName;
+
+	private  $channel;
+
+	public function getInstanceId() {
+		return $this->instanceId;
+	}
+
+	public function setInstanceId($instanceId) {
+		$this->instanceId = $instanceId;
+		$this->queryParameters["InstanceId"]=$instanceId;
+	}
+
+	public function getFileName() {
+		return $this->fileName;
+	}
+
+	public function setFileName($fileName) {
+		$this->fileName = $fileName;
+		$this->queryParameters["FileName"]=$fileName;
+	}
+
+	public function getChannel() {
+		return $this->channel;
+	}
+
+	public function setChannel($channel) {
+		$this->channel = $channel;
+		$this->queryParameters["Channel"]=$channel;
+	}
+	
+}
---
+++ aliyun-php-sdk-ccc/CCC/Request/V20170705/ListCallDetailRecordsRequest.php
@@ -0,0 +1,107 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace CCC\Request\V20170705;
+
+class ListCallDetailRecordsRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("CCC", "2017-07-05", "ListCallDetailRecords", "ccc", "openAPI");
+		$this->setMethod("POST");
+	}
+
+	private  $instanceId;
+
+	private  $criteria;
+
+	private  $phoneNumber;
+
+	private  $pageSize;
+
+	private  $startTime;
+
+	private  $stopTime;
+
+	private  $pageNumber;
+
+	public function getInstanceId() {
+		return $this->instanceId;
+	}
+
+	public function setInstanceId($instanceId) {
+		$this->instanceId = $instanceId;
+		$this->queryParameters["InstanceId"]=$instanceId;
+	}
+
+	public function getCriteria() {
+		return $this->criteria;
+	}
+
+	public function setCriteria($criteria) {
+		$this->criteria = $criteria;
+		$this->queryParameters["Criteria"]=$criteria;
+	}
+
+	public function getPhoneNumber() {
+		return $this->phoneNumber;
+	}
+
+	public function setPhoneNumber($phoneNumber) {
+		$this->phoneNumber = $phoneNumber;
+		$this->queryParameters["PhoneNumber"]=$phoneNumber;
+	}
+
+	public function getPageSize() {
+		return $this->pageSize;
+	}
+
+	public function setPageSize($pageSize) {
+		$this->pageSize = $pageSize;
+		$this->queryParameters["PageSize"]=$pageSize;
+	}
+
+	public function getStartTime() {
+		return $this->startTime;
+	}
+
+	public function setStartTime($startTime) {
+		$this->startTime = $startTime;
+		$this->queryParameters["StartTime"]=$startTime;
+	}
+
+	public function getStopTime() {
+		return $this->stopTime;
+	}
+
+	public function setStopTime($stopTime) {
+		$this->stopTime = $stopTime;
+		$this->queryParameters["StopTime"]=$stopTime;
+	}
+
+	public function getPageNumber() {
+		return $this->pageNumber;
+	}
+
+	public function setPageNumber($pageNumber) {
+		$this->pageNumber = $pageNumber;
+		$this->queryParameters["PageNumber"]=$pageNumber;
+	}
+	
+}
