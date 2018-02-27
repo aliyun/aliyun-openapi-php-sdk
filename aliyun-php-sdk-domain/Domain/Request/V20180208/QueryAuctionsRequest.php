f507e2eeeec67ef4314679bf2fdffac29b749a06
haowei.yao <haowei.yao@alibaba-inc.com>
2018-02-27 12:18:41 +0800

    由铁琴发起的DOMAIN SDK自动发布, BUILD_ID=444, 版本号：3.1.0 发布日志： 1, Add auction APIs. 2, Add return field DomainNameVerificationStatus to QueryDomainByInstanceId and QueryFailReasonForDomainRealNameVerification.

---
+++ aliyun-php-sdk-domain/Domain/Request/V20180208/QueryAuctionsRequest.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace Domain\Request\V20180208;
+
+class QueryAuctionsRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("Domain", "2018-02-08", "QueryAuctions");
+		$this->setMethod("POST");
+	}
+
+	private  $pageSize;
+
+	private  $currentPage;
+
+	private  $status;
+
+	public function getPageSize() {
+		return $this->pageSize;
+	}
+
+	public function setPageSize($pageSize) {
+		$this->pageSize = $pageSize;
+		}
+
+	public function getCurrentPage() {
+		return $this->currentPage;
+	}
+
+	public function setCurrentPage($currentPage) {
+		$this->currentPage = $currentPage;
+		}
+
+	public function getStatus() {
+		return $this->status;
+	}
+
+	public function setStatus($status) {
+		$this->status = $status;
+		}
+	
+}
---
+++ aliyun-php-sdk-domain/Domain/Request/V20180208/ReserveDomainRequest.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace Domain\Request\V20180208;
+
+class ReserveDomainRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("Domain", "2018-02-08", "ReserveDomain");
+		$this->setMethod("POST");
+	}
+
+	private  $Channelss;
+
+	private  $domainName;
+
+	public function getChannelss() {
+		return $this->Channelss;
+	}
+
+	public function setChannelss($Channelss) {
+		$this->Channelss = $Channelss;
+
+	}
+
+	public function getDomainName() {
+		return $this->domainName;
+	}
+
+	public function setDomainName($domainName) {
+		$this->domainName = $domainName;
+		}
+	
+}
