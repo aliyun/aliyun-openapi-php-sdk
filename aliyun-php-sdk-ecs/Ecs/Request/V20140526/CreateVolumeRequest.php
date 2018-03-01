910d7d992a91902fd8d53dc8c44d4c41df58fe5a
haowei.yao <haowei.yao@alibaba-inc.com>
2018-03-01 22:49:55 +0800

    由楚俊发起的ECS SDK自动发布, BUILD_ID=452, 版本号：4.6.4 发布日志： 1, DescribeInstanceTypes add eniQuantity vmBandwidthTx vmBandwidthRx attributes.

---
+++ aliyun-php-sdk-ecs/Ecs/Request/V20140526/CreateVolumeRequest.php
@@ -0,0 +1,272 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace Ecs\Request\V20140526;
+
+class CreateVolumeRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("Ecs", "2014-05-26", "CreateVolume", "ecs", "openAPI");
+		$this->setMethod("POST");
+	}
+
+	private  $tag4Value;
+
+	private  $resourceOwnerId;
+
+	private  $snapshotId;
+
+	private  $tag2Key;
+
+	private  $volumeName;
+
+	private  $volumeEncrypted;
+
+	private  $tag5Key;
+
+	private  $resourceOwnerAccount;
+
+	private  $clientToken;
+
+	private  $ownerAccount;
+
+	private  $description;
+
+	private  $tag3Key;
+
+	private  $ownerId;
+
+	private  $tag5Value;
+
+	private  $tag1Key;
+
+	private  $tag1Value;
+
+	private  $volumeCategory;
+
+	private  $size;
+
+	private  $tag2Value;
+
+	private  $zoneId;
+
+	private  $tag4Key;
+
+	private  $tag3Value;
+
+	public function getTag4Value() {
+		return $this->tag4Value;
+	}
+
+	public function setTag4Value($tag4Value) {
+		$this->tag4Value = $tag4Value;
+		$this->queryParameters["Tag.4.Value"]=$tag4Value;
+	}
+
+	public function getResourceOwnerId() {
+		return $this->resourceOwnerId;
+	}
+
+	public function setResourceOwnerId($resourceOwnerId) {
+		$this->resourceOwnerId = $resourceOwnerId;
+		$this->queryParameters["ResourceOwnerId"]=$resourceOwnerId;
+	}
+
+	public function getSnapshotId() {
+		return $this->snapshotId;
+	}
+
+	public function setSnapshotId($snapshotId) {
+		$this->snapshotId = $snapshotId;
+		$this->queryParameters["SnapshotId"]=$snapshotId;
+	}
+
+	public function getTag2Key() {
+		return $this->tag2Key;
+	}
+
+	public function setTag2Key($tag2Key) {
+		$this->tag2Key = $tag2Key;
+		$this->queryParameters["Tag.2.Key"]=$tag2Key;
+	}
+
+	public function getVolumeName() {
+		return $this->volumeName;
+	}
+
+	public function setVolumeName($volumeName) {
+		$this->volumeName = $volumeName;
+		$this->queryParameters["VolumeName"]=$volumeName;
+	}
+
+	public function getVolumeEncrypted() {
+		return $this->volumeEncrypted;
+	}
+
+	public function setVolumeEncrypted($volumeEncrypted) {
+		$this->volumeEncrypted = $volumeEncrypted;
+		$this->queryParameters["VolumeEncrypted"]=$volumeEncrypted;
+	}
+
+	public function getTag5Key() {
+		return $this->tag5Key;
+	}
+
+	public function setTag5Key($tag5Key) {
+		$this->tag5Key = $tag5Key;
+		$this->queryParameters["Tag.5.Key"]=$tag5Key;
+	}
+
+	public function getResourceOwnerAccount() {
+		return $this->resourceOwnerAccount;
+	}
+
+	public function setResourceOwnerAccount($resourceOwnerAccount) {
+		$this->resourceOwnerAccount = $resourceOwnerAccount;
+		$this->queryParameters["ResourceOwnerAccount"]=$resourceOwnerAccount;
+	}
+
+	public function getClientToken() {
+		return $this->clientToken;
+	}
+
+	public function setClientToken($clientToken) {
+		$this->clientToken = $clientToken;
+		$this->queryParameters["ClientToken"]=$clientToken;
+	}
+
+	public function getOwnerAccount() {
+		return $this->ownerAccount;
+	}
+
+	public function setOwnerAccount($ownerAccount) {
+		$this->ownerAccount = $ownerAccount;
+		$this->queryParameters["OwnerAccount"]=$ownerAccount;
+	}
+
+	public function getDescription() {
+		return $this->description;
+	}
+
+	public function setDescription($description) {
+		$this->description = $description;
+		$this->queryParameters["Description"]=$description;
+	}
+
+	public function getTag3Key() {
+		return $this->tag3Key;
+	}
+
+	public function setTag3Key($tag3Key) {
+		$this->tag3Key = $tag3Key;
+		$this->queryParameters["Tag.3.Key"]=$tag3Key;
+	}
+
+	public function getOwnerId() {
+		return $this->ownerId;
+	}
+
+	public function setOwnerId($ownerId) {
+		$this->ownerId = $ownerId;
+		$this->queryParameters["OwnerId"]=$ownerId;
+	}
+
+	public function getTag5Value() {
+		return $this->tag5Value;
+	}
+
+	public function setTag5Value($tag5Value) {
+		$this->tag5Value = $tag5Value;
+		$this->queryParameters["Tag.5.Value"]=$tag5Value;
+	}
+
+	public function getTag1Key() {
+		return $this->tag1Key;
+	}
+
+	public function setTag1Key($tag1Key) {
+		$this->tag1Key = $tag1Key;
+		$this->queryParameters["Tag.1.Key"]=$tag1Key;
+	}
+
+	public function getTag1Value() {
+		return $this->tag1Value;
+	}
+
+	public function setTag1Value($tag1Value) {
+		$this->tag1Value = $tag1Value;
+		$this->queryParameters["Tag.1.Value"]=$tag1Value;
+	}
+
+	public function getVolumeCategory() {
+		return $this->volumeCategory;
+	}
+
+	public function setVolumeCategory($volumeCategory) {
+		$this->volumeCategory = $volumeCategory;
+		$this->queryParameters["VolumeCategory"]=$volumeCategory;
+	}
+
+	public function getSize() {
+		return $this->size;
+	}
+
+	public function setSize($size) {
+		$this->size = $size;
+		$this->queryParameters["Size"]=$size;
+	}
+
+	public function getTag2Value() {
+		return $this->tag2Value;
+	}
+
+	public function setTag2Value($tag2Value) {
+		$this->tag2Value = $tag2Value;
+		$this->queryParameters["Tag.2.Value"]=$tag2Value;
+	}
+
+	public function getZoneId() {
+		return $this->zoneId;
+	}
+
+	public function setZoneId($zoneId) {
+		$this->zoneId = $zoneId;
+		$this->queryParameters["ZoneId"]=$zoneId;
+	}
+
+	public function getTag4Key() {
+		return $this->tag4Key;
+	}
+
+	public function setTag4Key($tag4Key) {
+		$this->tag4Key = $tag4Key;
+		$this->queryParameters["Tag.4.Key"]=$tag4Key;
+	}
+
+	public function getTag3Value() {
+		return $this->tag3Value;
+	}
+
+	public function setTag3Value($tag3Value) {
+		$this->tag3Value = $tag3Value;
+		$this->queryParameters["Tag.3.Value"]=$tag3Value;
+	}
+	
+}
---
+++ aliyun-php-sdk-ecs/Ecs/Request/V20140526/ModifyVolumeAttributeRequest.php
@@ -0,0 +1,107 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace Ecs\Request\V20140526;
+
+class ModifyVolumeAttributeRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("Ecs", "2014-05-26", "ModifyVolumeAttribute", "ecs", "openAPI");
+		$this->setMethod("POST");
+	}
+
+	private  $resourceOwnerId;
+
+	private  $volumeName;
+
+	private  $resourceOwnerAccount;
+
+	private  $ownerAccount;
+
+	private  $volumeId;
+
+	private  $description;
+
+	private  $ownerId;
+
+	public function getResourceOwnerId() {
+		return $this->resourceOwnerId;
+	}
+
+	public function setResourceOwnerId($resourceOwnerId) {
+		$this->resourceOwnerId = $resourceOwnerId;
+		$this->queryParameters["ResourceOwnerId"]=$resourceOwnerId;
+	}
+
+	public function getVolumeName() {
+		return $this->volumeName;
+	}
+
+	public function setVolumeName($volumeName) {
+		$this->volumeName = $volumeName;
+		$this->queryParameters["VolumeName"]=$volumeName;
+	}
+
+	public function getResourceOwnerAccount() {
+		return $this->resourceOwnerAccount;
+	}
+
+	public function setResourceOwnerAccount($resourceOwnerAccount) {
+		$this->resourceOwnerAccount = $resourceOwnerAccount;
+		$this->queryParameters["ResourceOwnerAccount"]=$resourceOwnerAccount;
+	}
+
+	public function getOwnerAccount() {
+		return $this->ownerAccount;
+	}
+
+	public function setOwnerAccount($ownerAccount) {
+		$this->ownerAccount = $ownerAccount;
+		$this->queryParameters["OwnerAccount"]=$ownerAccount;
+	}
+
+	public function getVolumeId() {
+		return $this->volumeId;
+	}
+
+	public function setVolumeId($volumeId) {
+		$this->volumeId = $volumeId;
+		$this->queryParameters["VolumeId"]=$volumeId;
+	}
+
+	public function getDescription() {
+		return $this->description;
+	}
+
+	public function setDescription($description) {
+		$this->description = $description;
+		$this->queryParameters["Description"]=$description;
+	}
+
+	public function getOwnerId() {
+		return $this->ownerId;
+	}
+
+	public function setOwnerId($ownerId) {
+		$this->ownerId = $ownerId;
+		$this->queryParameters["OwnerId"]=$ownerId;
+	}
+	
+}
