a488dbebb1a25c389f839a7c40ff7f5fe87c3fc3
haowei.yao <haowei.yao@alibaba-inc.com>
2018-03-27 10:25:04 +0800

    由廷诚发起的CLOUDPHOTO SDK自动发布, BUILD_ID=502, 版本号：1.1.14 发布日志： 1, add interface FetchLibraries

---
+++ aliyun-php-sdk-cloudphoto/CloudPhoto/Request/V20170711/FetchLibrariesRequest.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace CloudPhoto\Request\V20170711;
+
+class FetchLibrariesRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("CloudPhoto", "2017-07-11", "FetchLibraries", "cloudphoto", "openAPI");
+		$this->setProtocol("https");
+		$this->setMethod("POST");
+	}
+
+	private  $size;
+
+	private  $storeName;
+
+	private  $page;
+
+	public function getSize() {
+		return $this->size;
+	}
+
+	public function setSize($size) {
+		$this->size = $size;
+		$this->queryParameters["Size"]=$size;
+	}
+
+	public function getStoreName() {
+		return $this->storeName;
+	}
+
+	public function setStoreName($storeName) {
+		$this->storeName = $storeName;
+		$this->queryParameters["StoreName"]=$storeName;
+	}
+
+	public function getPage() {
+		return $this->page;
+	}
+
+	public function setPage($page) {
+		$this->page = $page;
+		$this->queryParameters["Page"]=$page;
+	}
+	
+}
