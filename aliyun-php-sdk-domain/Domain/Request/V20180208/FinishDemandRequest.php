b7f5e006b685c5690e9b7ab6bd68f206732aa638
haowei.yao <haowei.yao@alibaba-inc.com>
2018-05-09 12:50:21 +0800

    由铁琴发起的DOMAIN SDK自动发布, 版本号：3.5.0 发布日志： 1, Add apis for domain broker.

---
+++ aliyun-php-sdk-domain/Domain/Request/V20180208/FinishDemandRequest.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace Domain\Request\V20180208;
+
+class FinishDemandRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("Domain", "2018-02-08", "FinishDemand");
+		$this->setMethod("POST");
+	}
+
+	private  $bizId;
+
+	private  $message;
+
+	public function getBizId() {
+		return $this->bizId;
+	}
+
+	public function setBizId($bizId) {
+		$this->bizId = $bizId;
+		$this->queryParameters["BizId"]=$bizId;
+	}
+
+	public function getMessage() {
+		return $this->message;
+	}
+
+	public function setMessage($message) {
+		$this->message = $message;
+		$this->queryParameters["Message"]=$message;
+	}
+	
+}
