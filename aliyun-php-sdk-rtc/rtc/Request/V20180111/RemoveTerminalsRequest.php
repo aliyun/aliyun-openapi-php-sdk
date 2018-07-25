50977a4e5fa0568601175011501bdb530329cd5a
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-07-25 15:49:41 +0800

    RTC SDK Auto Released By renrang.yl,Version：0.8.1 发布日志： 1, Add RemoveTerminals API.

---
+++ aliyun-php-sdk-rtc/rtc/Request/V20180111/RemoveTerminalsRequest.php
@@ -0,0 +1,76 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace rtc\Request\V20180111;
+
+class RemoveTerminalsRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("rtc", "2018-01-11", "RemoveTerminals", "rtc", "openAPI");
+		$this->setMethod("POST");
+	}
+
+	private  $TerminalIdss;
+
+	private  $ownerId;
+
+	private  $appId;
+
+	private  $channelId;
+
+	public function getTerminalIdss() {
+		return $this->TerminalIdss;
+	}
+
+	public function setTerminalIdss($TerminalIdss) {
+		$this->TerminalIdss = $TerminalIdss;
+		for ($i = 0; $i < count($TerminalIdss); $i ++) {	
+			$this->queryParameters["TerminalIds.".($i+1)] = $TerminalIdss[$i];
+		}
+	}
+
+	public function getOwnerId() {
+		return $this->ownerId;
+	}
+
+	public function setOwnerId($ownerId) {
+		$this->ownerId = $ownerId;
+		$this->queryParameters["OwnerId"]=$ownerId;
+	}
+
+	public function getAppId() {
+		return $this->appId;
+	}
+
+	public function setAppId($appId) {
+		$this->appId = $appId;
+		$this->queryParameters["AppId"]=$appId;
+	}
+
+	public function getChannelId() {
+		return $this->channelId;
+	}
+
+	public function setChannelId($channelId) {
+		$this->channelId = $channelId;
+		$this->queryParameters["ChannelId"]=$channelId;
+	}
+	
+}
