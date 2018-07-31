9f903407a0c99110552c7433f71626d1a8fb62f0
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-08-01 00:43:03 +0800

    MOPEN SDK Auto Released By everettli.ll,Version：1.1.0 发布日志： 1, Add api: MoPenQueryCanvas, MoPenDoRecognize, MoPenSendMqttMessage, MoPenFindGroup

---
+++ aliyun-php-sdk-mopen/MoPen/Request/V20180211/MoPenDoRecognizeRequest.php
@@ -0,0 +1,101 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace MoPen\Request\V20180211;
+
+class MoPenDoRecognizeRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("MoPen", "2018-02-11", "MoPenDoRecognize");
+		$this->setProtocol("https");
+		$this->setMethod("POST");
+	}
+
+	private  $canvasId;
+
+	private  $endY;
+
+	private  $endX;
+
+	private  $jsonConf;
+
+	private  $exportType;
+
+	private  $startY;
+
+	private  $startX;
+
+	public function getCanvasId() {
+		return $this->canvasId;
+	}
+
+	public function setCanvasId($canvasId) {
+		$this->canvasId = $canvasId;
+		}
+
+	public function getEndY() {
+		return $this->endY;
+	}
+
+	public function setEndY($endY) {
+		$this->endY = $endY;
+		}
+
+	public function getEndX() {
+		return $this->endX;
+	}
+
+	public function setEndX($endX) {
+		$this->endX = $endX;
+		}
+
+	public function getJsonConf() {
+		return $this->jsonConf;
+	}
+
+	public function setJsonConf($jsonConf) {
+		$this->jsonConf = $jsonConf;
+		}
+
+	public function getExportType() {
+		return $this->exportType;
+	}
+
+	public function setExportType($exportType) {
+		$this->exportType = $exportType;
+		}
+
+	public function getStartY() {
+		return $this->startY;
+	}
+
+	public function setStartY($startY) {
+		$this->startY = $startY;
+		}
+
+	public function getStartX() {
+		return $this->startX;
+	}
+
+	public function setStartX($startX) {
+		$this->startX = $startX;
+		}
+	
+}
---
+++ aliyun-php-sdk-mopen/MoPen/Request/V20180211/MoPenQueryCanvasRequest.php
@@ -0,0 +1,71 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace MoPen\Request\V20180211;
+
+class MoPenQueryCanvasRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("MoPen", "2018-02-11", "MoPenQueryCanvas");
+		$this->setProtocol("https");
+		$this->setMethod("POST");
+	}
+
+	private  $deviceName;
+
+	private  $sessionId;
+
+	private  $pageId;
+
+	private  $status;
+
+	public function getDeviceName() {
+		return $this->deviceName;
+	}
+
+	public function setDeviceName($deviceName) {
+		$this->deviceName = $deviceName;
+		}
+
+	public function getSessionId() {
+		return $this->sessionId;
+	}
+
+	public function setSessionId($sessionId) {
+		$this->sessionId = $sessionId;
+		}
+
+	public function getPageId() {
+		return $this->pageId;
+	}
+
+	public function setPageId($pageId) {
+		$this->pageId = $pageId;
+		}
+
+	public function getStatus() {
+		return $this->status;
+	}
+
+	public function setStatus($status) {
+		$this->status = $status;
+		}
+	
+}
