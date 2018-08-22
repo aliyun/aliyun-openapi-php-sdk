ee61239841ea7119fcef27a889e7415fadd65072
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-08-22 13:02:09 +0800

    ECS SDK Auto Released By wenle.bwl,Version：4.10.0 发布日志： 1, Add api CreateSimulatedSystemEvents, support creating one or more simulated system events. 2, Add api CancelSimulatedSystemEvents, support cancelling one or more simulated system events.

---
+++ aliyun-php-sdk-ecs/Ecs/Request/V20140526/CancelSimulatedSystemEventsRequest.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace Ecs\Request\V20140526;
+
+class CancelSimulatedSystemEventsRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("Ecs", "2014-05-26", "CancelSimulatedSystemEvents", "ecs", "openAPI");
+		$this->setMethod("POST");
+	}
+
+	private  $EventIds;
+
+	private  $resourceOwnerId;
+
+	private  $resourceOwnerAccount;
+
+	private  $ownerAccount;
+
+	private  $ownerId;
+
+	public function getEventIds() {
+		return $this->EventIds;
+	}
+
+	public function setEventIds($EventIds) {
+		$this->EventIds = $EventIds;
+		for ($i = 0; $i < count($EventIds); $i ++) {	
+			$this->queryParameters["EventId.".($i+1)] = $EventIds[$i];
+		}
+	}
+
+	public function getResourceOwnerId() {
+		return $this->resourceOwnerId;
+	}
+
+	public function setResourceOwnerId($resourceOwnerId) {
+		$this->resourceOwnerId = $resourceOwnerId;
+		$this->queryParameters["ResourceOwnerId"]=$resourceOwnerId;
+	}
+
+	public function getResourceOwnerAccount() {
+		return $this->resourceOwnerAccount;
+	}
+
+	public function setResourceOwnerAccount($resourceOwnerAccount) {
+		$this->resourceOwnerAccount = $resourceOwnerAccount;
+		$this->queryParameters["ResourceOwnerAccount"]=$resourceOwnerAccount;
+	}
+
+	public function getOwnerAccount() {
+		return $this->ownerAccount;
+	}
+
+	public function setOwnerAccount($ownerAccount) {
+		$this->ownerAccount = $ownerAccount;
+		$this->queryParameters["OwnerAccount"]=$ownerAccount;
+	}
+
+	public function getOwnerId() {
+		return $this->ownerId;
+	}
+
+	public function setOwnerId($ownerId) {
+		$this->ownerId = $ownerId;
+		$this->queryParameters["OwnerId"]=$ownerId;
+	}
+	
+}
