e7d893f6bbd70e9f665858239a34848f42ccb8e5
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-12-15 09:44:49 +0800

    HTTPDNS SDK Auto Released By baijue.xwz,Version：2.2.0 发布日志： 1, first release 2, this SDK is for control purposes, programmatic interface of HTTPDNS console: https://httpdns.console.aliyun.com

---
+++ aliyun-php-sdk-httpdns/Httpdns/Request/V20160201/GetResolveStatisticsRequest.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+namespace Httpdns\Request\V20160201;
+
+class GetResolveStatisticsRequest extends \RpcAcsRequest
+{
+	function  __construct()
+	{
+		parent::__construct("Httpdns", "2016-02-01", "GetResolveStatistics");
+		$this->setMethod("POST");
+	}
+
+	private  $granularity;
+
+	private  $protocolName;
+
+	private  $domainName;
+
+	private  $timeSpan;
+
+	public function getGranularity() {
+		return $this->granularity;
+	}
+
+	public function setGranularity($granularity) {
+		$this->granularity = $granularity;
+		$this->queryParameters["Granularity"]=$granularity;
+	}
+
+	public function getProtocolName() {
+		return $this->protocolName;
+	}
+
+	public function setProtocolName($protocolName) {
+		$this->protocolName = $protocolName;
+		$this->queryParameters["ProtocolName"]=$protocolName;
+	}
+
+	public function getDomainName() {
+		return $this->domainName;
+	}
+
+	public function setDomainName($domainName) {
+		$this->domainName = $domainName;
+		$this->queryParameters["DomainName"]=$domainName;
+	}
+
+	public function getTimeSpan() {
+		return $this->timeSpan;
+	}
+
+	public function setTimeSpan($timeSpan) {
+		$this->timeSpan = $timeSpan;
+		$this->queryParameters["TimeSpan"]=$timeSpan;
+	}
+	
+}
