7934356f7bf0deccdfbd961a2904b533ee9bd6d3
haowei.yao <haowei.yao@alibaba-inc.com>
2019-03-14 13:28:17 +0800

    STS SDK Auto Released By shenshi,Version：3.0.1 发布日志： 1, Update Dependency

--- aliyun-php-sdk-sts/Sts/Request/V20150401/GenerateSessionAccessKeyRequest.php
+++ aliyun-php-sdk-sts/Sts/Request/V20150401/GenerateSessionAccessKeyRequest.php
@@ -1,42 +1,48 @@
 <?php
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 namespace Sts\Request\V20150401;
 
+/**
+ * Request of GenerateSessionAccessKey
+ *
+ * @method string getDurationSeconds()
+ */
 class GenerateSessionAccessKeyRequest extends \RpcAcsRequest
 {
-	function  __construct()
-	{
-		parent::__construct("Sts", "2015-04-01", "GenerateSessionAccessKey");
-		$this->setProtocol("https");
-		$this->setMethod("POST");
-	}
 
-	private  $durationSeconds;
+    /**
+     * @var string
+     */
+    protected $requestScheme = 'https';
 
-	public function getDurationSeconds() {
-		return $this->durationSeconds;
-	}
+    /**
+     * @var string
+     */
+    protected $method = 'POST';
 
-	public function setDurationSeconds($durationSeconds) {
-		$this->durationSeconds = $durationSeconds;
-		$this->queryParameters["DurationSeconds"]=$durationSeconds;
-	}
-	
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            'Sts',
+            '2015-04-01',
+            'GenerateSessionAccessKey',
+            'sts'
+        );
+    }
+
+    /**
+     * @param string $durationSeconds
+     *
+     * @return $this
+     */
+    public function setDurationSeconds($durationSeconds)
+    {
+        $this->requestParameters['DurationSeconds'] = $durationSeconds;
+        $this->queryParameters['DurationSeconds'] = $durationSeconds;
+
+        return $this;
+    }
 }
