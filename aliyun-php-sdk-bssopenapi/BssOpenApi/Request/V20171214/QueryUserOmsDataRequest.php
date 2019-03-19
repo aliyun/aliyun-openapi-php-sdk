b20b0bcecc114f323777a87526d7f342e7bc7858
haowei.yao <haowei.yao@alibaba-inc.com>
2019-03-19 15:44:17 +0800

    BSSOPENAPI SDK Auto Released By simon.lb,Version：1.2.2 发布日志： 1, add query user oms data api

---
+++ aliyun-php-sdk-bssopenapi/BssOpenApi/Request/V20171214/QueryUserOmsDataRequest.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace BssOpenApi\Request\V20171214;
+
+/**
+ * @deprecated Please use https://github.com/aliyun/openapi-sdk-php
+ *
+ * Request of QueryUserOmsData
+ *
+ * @method string getDataType()
+ * @method string getMarker()
+ * @method string getPageSize()
+ * @method string getEndTime()
+ * @method string getStartTime()
+ * @method string getOwnerId()
+ * @method string getTable()
+ */
+class QueryUserOmsDataRequest extends \RpcAcsRequest
+{
+
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            'BssOpenApi',
+            '2017-12-14',
+            'QueryUserOmsData'
+        );
+    }
+
+    /**
+     * @param string $dataType
+     *
+     * @return $this
+     */
+    public function setDataType($dataType)
+    {
+        $this->requestParameters['DataType'] = $dataType;
+        $this->queryParameters['DataType'] = $dataType;
+
+        return $this;
+    }
+
+    /**
+     * @param string $marker
+     *
+     * @return $this
+     */
+    public function setMarker($marker)
+    {
+        $this->requestParameters['Marker'] = $marker;
+        $this->queryParameters['Marker'] = $marker;
+
+        return $this;
+    }
+
+    /**
+     * @param string $pageSize
+     *
+     * @return $this
+     */
+    public function setPageSize($pageSize)
+    {
+        $this->requestParameters['PageSize'] = $pageSize;
+        $this->queryParameters['PageSize'] = $pageSize;
+
+        return $this;
+    }
+
+    /**
+     * @param string $endTime
+     *
+     * @return $this
+     */
+    public function setEndTime($endTime)
+    {
+        $this->requestParameters['EndTime'] = $endTime;
+        $this->queryParameters['EndTime'] = $endTime;
+
+        return $this;
+    }
+
+    /**
+     * @param string $startTime
+     *
+     * @return $this
+     */
+    public function setStartTime($startTime)
+    {
+        $this->requestParameters['StartTime'] = $startTime;
+        $this->queryParameters['StartTime'] = $startTime;
+
+        return $this;
+    }
+
+    /**
+     * @param string $ownerId
+     *
+     * @return $this
+     */
+    public function setOwnerId($ownerId)
+    {
+        $this->requestParameters['OwnerId'] = $ownerId;
+        $this->queryParameters['OwnerId'] = $ownerId;
+
+        return $this;
+    }
+
+    /**
+     * @param string $table
+     *
+     * @return $this
+     */
+    public function setTable($table)
+    {
+        $this->requestParameters['Table'] = $table;
+        $this->queryParameters['Table'] = $table;
+
+        return $this;
+    }
+}
