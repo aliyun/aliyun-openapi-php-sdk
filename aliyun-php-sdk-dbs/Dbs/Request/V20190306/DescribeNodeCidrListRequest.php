6f46b75d63deb5c82c463ae9df657454cac1df65
haowei.yao <haowei.yao@alibaba-inc.com>
2019-04-17 00:26:59 +0800

    DBS SDK Auto Released By wanxin,Version：1.0.5 发布日志： 1, Add DescribeRestoreTaskList DBS interface. 2, Add DescribeNodeCidrList DBS interface.

---
+++ aliyun-php-sdk-dbs/Dbs/Request/V20190306/DescribeNodeCidrListRequest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Dbs\Request\V20190306;
+
+/**
+ * @deprecated Please use https://github.com/aliyun/openapi-sdk-php
+ *
+ * Request of DescribeNodeCidrList
+ *
+ * @method string getClientToken()
+ * @method string getRegion()
+ * @method string getOwnerId()
+ */
+class DescribeNodeCidrListRequest extends \RpcAcsRequest
+{
+
+    /**
+     * @var string
+     */
+    protected $method = 'POST';
+
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            'Dbs',
+            '2019-03-06',
+            'DescribeNodeCidrList',
+            'cbs'
+        );
+    }
+
+    /**
+     * @param string $clientToken
+     *
+     * @return $this
+     */
+    public function setClientToken($clientToken)
+    {
+        $this->requestParameters['ClientToken'] = $clientToken;
+        $this->queryParameters['ClientToken'] = $clientToken;
+
+        return $this;
+    }
+
+    /**
+     * @param string $region
+     *
+     * @return $this
+     */
+    public function setRegion($region)
+    {
+        $this->requestParameters['Region'] = $region;
+        $this->queryParameters['Region'] = $region;
+
+        return $this;
+    }
+
+    /**
+     * @param string $ownerId
+     *
+     * @return $this
+     */
+    public function setOwnerId($ownerId)
+    {
+        $this->requestParameters['OwnerId'] = $ownerId;
+        $this->queryParameters['OwnerId'] = $ownerId;
+
+        return $this;
+    }
+}
---
+++ aliyun-php-sdk-dbs/Dbs/Request/V20190306/DescribeRestoreTaskListRequest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Dbs\Request\V20190306;
+
+/**
+ * @deprecated Please use https://github.com/aliyun/openapi-sdk-php
+ *
+ * Request of DescribeRestoreTaskList
+ *
+ * @method string getClientToken()
+ * @method string getRestoreTaskId()
+ * @method string getPageSize()
+ * @method string getBackupPlanId()
+ * @method string getPageNum()
+ * @method string getOwnerId()
+ */
+class DescribeRestoreTaskListRequest extends \RpcAcsRequest
+{
+
+    /**
+     * @var string
+     */
+    protected $method = 'POST';
+
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            'Dbs',
+            '2019-03-06',
+            'DescribeRestoreTaskList',
+            'cbs'
+        );
+    }
+
+    /**
+     * @param string $clientToken
+     *
+     * @return $this
+     */
+    public function setClientToken($clientToken)
+    {
+        $this->requestParameters['ClientToken'] = $clientToken;
+        $this->queryParameters['ClientToken'] = $clientToken;
+
+        return $this;
+    }
+
+    /**
+     * @param string $restoreTaskId
+     *
+     * @return $this
+     */
+    public function setRestoreTaskId($restoreTaskId)
+    {
+        $this->requestParameters['RestoreTaskId'] = $restoreTaskId;
+        $this->queryParameters['RestoreTaskId'] = $restoreTaskId;
+
+        return $this;
+    }
+
+    /**
+     * @param string $pageSize
+     *
+     * @return $this
+     */
+    public function setPageSize($pageSize)
+    {
+        $this->requestParameters['PageSize'] = $pageSize;
+        $this->queryParameters['PageSize'] = $pageSize;
+
+        return $this;
+    }
+
+    /**
+     * @param string $backupPlanId
+     *
+     * @return $this
+     */
+    public function setBackupPlanId($backupPlanId)
+    {
+        $this->requestParameters['BackupPlanId'] = $backupPlanId;
+        $this->queryParameters['BackupPlanId'] = $backupPlanId;
+
+        return $this;
+    }
+
+    /**
+     * @param string $pageNum
+     *
+     * @return $this
+     */
+    public function setPageNum($pageNum)
+    {
+        $this->requestParameters['PageNum'] = $pageNum;
+        $this->queryParameters['PageNum'] = $pageNum;
+
+        return $this;
+    }
+
+    /**
+     * @param string $ownerId
+     *
+     * @return $this
+     */
+    public function setOwnerId($ownerId)
+    {
+        $this->requestParameters['OwnerId'] = $ownerId;
+        $this->queryParameters['OwnerId'] = $ownerId;
+
+        return $this;
+    }
+}
