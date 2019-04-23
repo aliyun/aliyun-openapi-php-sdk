2190eb9a99a804cda319ffbcadea75da05ed904b
haowei.yao <haowei.yao@alibaba-inc.com>
2019-04-23 16:54:36 +0800

    EHPC SDK Auto Released By qianzheng.llc,Version：1.11.1 发布日志： 1, Support mounting multiple volumes using different protocol 2, Support scaling out cross AZ

--- aliyun-php-sdk-ehpc/EHPC/Request/V20180412/SetAutoScaleConfigRequest.php
+++ aliyun-php-sdk-ehpc/EHPC/Request/V20180412/SetAutoScaleConfigRequest.php
@@ -181,7 +181,9 @@ class SetAutoScaleConfigRequest extends \RpcAcsRequest
             $this->queryParameters['Queues.' . ($depth1 + 1) . '.QueueName'] = $depth1Value['QueueName'];
             foreach ($depth1Value['InstanceTypes'] as $depth2 => $depth2Value) {
                 $this->queryParameters['Queues.' . ($depth1 + 1) . '.InstanceTypes.' . ($depth2 + 1) . '.SpotStrategy'] = $depth2Value['SpotStrategy'];
+                $this->queryParameters['Queues.' . ($depth1 + 1) . '.InstanceTypes.' . ($depth2 + 1) . '.VSwitchId'] = $depth2Value['VSwitchId'];
                 $this->queryParameters['Queues.' . ($depth1 + 1) . '.InstanceTypes.' . ($depth2 + 1) . '.InstanceType'] = $depth2Value['InstanceType'];
+                $this->queryParameters['Queues.' . ($depth1 + 1) . '.InstanceTypes.' . ($depth2 + 1) . '.ZoneId'] = $depth2Value['ZoneId'];
                 $this->queryParameters['Queues.' . ($depth1 + 1) . '.InstanceTypes.' . ($depth2 + 1) . '.SpotPriceLimit'] = $depth2Value['SpotPriceLimit'];
             }
             $this->queryParameters['Queues.' . ($depth1 + 1) . '.InstanceType'] = $depth1Value['InstanceType'];
