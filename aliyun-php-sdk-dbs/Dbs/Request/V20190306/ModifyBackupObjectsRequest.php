40f1ea0bee5779b9ff8e3397f0ec49c6beb4399d
sdk-team <sdk-team@alibabacloud.com>
2019-05-18 23:13:19 +0800

    DBS SDK Auto Released By wanxin,Version：1.0.7 发布日志： 1, Add ModifyBackupObjects DBS interface.

---
+++ aliyun-php-sdk-dbs/Dbs/Request/V20190306/ModifyBackupObjectsRequest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Dbs\Request\V20190306;
+
+/**
+ * @deprecated Please use https://github.com/aliyun/openapi-sdk-php
+ *
+ * Request of ModifyBackupObjects
+ *
+ * @method string getClientToken()
+ * @method string getBackupPlanId()
+ * @method string getBackupObjects()
+ * @method string getOwnerId()
+ */
+class ModifyBackupObjectsRequest extends \RpcAcsRequest
+{
+
+    /**
+     * @var string
+     */
+    protected $method = 'POST';
+
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            'Dbs',
+            '2019-03-06',
+            'ModifyBackupObjects',
+            'cbs'
+        );
+    }
+
+    /**
+     * @param string $clientToken
+     *
+     * @return $this
+     */
+    public function setClientToken($clientToken)
+    {
+        $this->requestParameters['ClientToken'] = $clientToken;
+        $this->queryParameters['ClientToken'] = $clientToken;
+
+        return $this;
+    }
+
+    /**
+     * @param string $backupPlanId
+     *
+     * @return $this
+     */
+    public function setBackupPlanId($backupPlanId)
+    {
+        $this->requestParameters['BackupPlanId'] = $backupPlanId;
+        $this->queryParameters['BackupPlanId'] = $backupPlanId;
+
+        return $this;
+    }
+
+    /**
+     * @param string $backupObjects
+     *
+     * @return $this
+     */
+    public function setBackupObjects($backupObjects)
+    {
+        $this->requestParameters['BackupObjects'] = $backupObjects;
+        $this->queryParameters['BackupObjects'] = $backupObjects;
+
+        return $this;
+    }
+
+    /**
+     * @param string $ownerId
+     *
+     * @return $this
+     */
+    public function setOwnerId($ownerId)
+    {
+        $this->requestParameters['OwnerId'] = $ownerId;
+        $this->queryParameters['OwnerId'] = $ownerId;
+
+        return $this;
+    }
+}
