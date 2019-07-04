f8c7c7d8252a5da68fc439acd9bb617b74e7f204
sdk-team <sdk-team@alibabacloud.com>
2019-07-04 14:08:27 +0800

    Supported API DescribeRecordStatisticsy for Query Volume.

---
+++ aliyun-php-sdk-alidns/Alidns/Request/V20150109/DescribeDomainStatisticsRequest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Alidns\Request\V20150109;
+
+/**
+ * @deprecated Please use https://github.com/aliyun/openapi-sdk-php
+ *
+ * Request of DescribeDomainStatistics
+ *
+ * @method string getEndDate()
+ * @method string getUserClientIp()
+ * @method string getDomainName()
+ * @method string getLang()
+ * @method string getStartDate()
+ */
+class DescribeDomainStatisticsRequest extends \RpcAcsRequest
+{
+
+    /**
+     * @var string
+     */
+    protected $method = 'POST';
+
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            'Alidns',
+            '2015-01-09',
+            'DescribeDomainStatistics',
+            'Alidns'
+        );
+    }
+
+    /**
+     * @param string $endDate
+     *
+     * @return $this
+     */
+    public function setEndDate($endDate)
+    {
+        $this->requestParameters['EndDate'] = $endDate;
+        $this->queryParameters['EndDate'] = $endDate;
+
+        return $this;
+    }
+
+    /**
+     * @param string $userClientIp
+     *
+     * @return $this
+     */
+    public function setUserClientIp($userClientIp)
+    {
+        $this->requestParameters['UserClientIp'] = $userClientIp;
+        $this->queryParameters['UserClientIp'] = $userClientIp;
+
+        return $this;
+    }
+
+    /**
+     * @param string $domainName
+     *
+     * @return $this
+     */
+    public function setDomainName($domainName)
+    {
+        $this->requestParameters['DomainName'] = $domainName;
+        $this->queryParameters['DomainName'] = $domainName;
+
+        return $this;
+    }
+
+    /**
+     * @param string $lang
+     *
+     * @return $this
+     */
+    public function setLang($lang)
+    {
+        $this->requestParameters['Lang'] = $lang;
+        $this->queryParameters['Lang'] = $lang;
+
+        return $this;
+    }
+
+    /**
+     * @param string $startDate
+     *
+     * @return $this
+     */
+    public function setStartDate($startDate)
+    {
+        $this->requestParameters['StartDate'] = $startDate;
+        $this->queryParameters['StartDate'] = $startDate;
+
+        return $this;
+    }
+}
