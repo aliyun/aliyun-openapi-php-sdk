619d07aa5971535e5581d710226f542765d7eea3
sdk-team <sdk-team@alibabacloud.com>
2019-07-11 15:36:53 +0800

    Modify CreateBackupPlan.

--- aliyun-php-sdk-dbs/Dbs/Request/V20190306/CreateBackupPlanRequest.php
+++ aliyun-php-sdk-dbs/Dbs/Request/V20190306/CreateBackupPlanRequest.php
@@ -7,14 +7,19 @@ namespace Dbs\Request\V20190306;
  *
  * Request of CreateBackupPlan
  *
- * @method string getBackupMethod()
  * @method string getDatabaseType()
  * @method string getPeriod()
  * @method string getClientToken()
- * @method string getRegion()
  * @method string getOwnerId()
  * @method string getUsedTime()
  * @method string getInstanceClass()
+ * @method string getStorageType()
+ * @method string getBackupMethod()
+ * @method string getDatabaseRegion()
+ * @method string getStorageRegion()
+ * @method string getInstanceType()
+ * @method string getRegion()
+ * @method string getPayType()
  */
 class CreateBackupPlanRequest extends \RpcAcsRequest
 {
@@ -35,19 +40,6 @@ class CreateBackupPlanRequest extends \RpcAcsRequest
             'CreateBackupPlan',
             'cbs'
         );
-    }
-
-    /**
-     * @param string $backupMethod
-     *
-     * @return $this
-     */
-    public function setBackupMethod($backupMethod)
-    {
-        $this->requestParameters['BackupMethod'] = $backupMethod;
-        $this->queryParameters['BackupMethod'] = $backupMethod;
-
-        return $this;
     }
 
     /**
@@ -90,19 +82,6 @@ class CreateBackupPlanRequest extends \RpcAcsRequest
     }
 
     /**
-     * @param string $region
-     *
-     * @return $this
-     */
-    public function setRegion($region)
-    {
-        $this->requestParameters['Region'] = $region;
-        $this->queryParameters['Region'] = $region;
-
-        return $this;
-    }
-
-    /**
      * @param string $ownerId
      *
      * @return $this
@@ -137,6 +116,97 @@ class CreateBackupPlanRequest extends \RpcAcsRequest
     {
         $this->requestParameters['InstanceClass'] = $instanceClass;
         $this->queryParameters['InstanceClass'] = $instanceClass;
+
+        return $this;
+    }
+
+    /**
+     * @param string $storageType
+     *
+     * @return $this
+     */
+    public function setStorageType($storageType)
+    {
+        $this->requestParameters['StorageType'] = $storageType;
+        $this->queryParameters['StorageType'] = $storageType;
+
+        return $this;
+    }
+
+    /**
+     * @param string $backupMethod
+     *
+     * @return $this
+     */
+    public function setBackupMethod($backupMethod)
+    {
+        $this->requestParameters['BackupMethod'] = $backupMethod;
+        $this->queryParameters['BackupMethod'] = $backupMethod;
+
+        return $this;
+    }
+
+    /**
+     * @param string $databaseRegion
+     *
+     * @return $this
+     */
+    public function setDatabaseRegion($databaseRegion)
+    {
+        $this->requestParameters['DatabaseRegion'] = $databaseRegion;
+        $this->queryParameters['DatabaseRegion'] = $databaseRegion;
+
+        return $this;
+    }
+
+    /**
+     * @param string $storageRegion
+     *
+     * @return $this
+     */
+    public function setStorageRegion($storageRegion)
+    {
+        $this->requestParameters['StorageRegion'] = $storageRegion;
+        $this->queryParameters['StorageRegion'] = $storageRegion;
+
+        return $this;
+    }
+
+    /**
+     * @param string $instanceType
+     *
+     * @return $this
+     */
+    public function setInstanceType($instanceType)
+    {
+        $this->requestParameters['InstanceType'] = $instanceType;
+        $this->queryParameters['InstanceType'] = $instanceType;
+
+        return $this;
+    }
+
+    /**
+     * @param string $region
+     *
+     * @return $this
+     */
+    public function setRegion($region)
+    {
+        $this->requestParameters['Region'] = $region;
+        $this->queryParameters['Region'] = $region;
+
+        return $this;
+    }
+
+    /**
+     * @param string $payType
+     *
+     * @return $this
+     */
+    public function setPayType($payType)
+    {
+        $this->requestParameters['PayType'] = $payType;
+        $this->queryParameters['PayType'] = $payType;
 
         return $this;
     }
---
+++ aliyun-php-sdk-dbs/Dbs/Request/V20190306/ModifyBackupStrategyRequest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Dbs\Request\V20190306;
+
+/**
+ * @deprecated Please use https://github.com/aliyun/openapi-sdk-php
+ *
+ * Request of ModifyBackupStrategy
+ *
+ * @method string getBackupPeriod()
+ * @method string getBackupStartTime()
+ * @method string getClientToken()
+ * @method string getBackupPlanId()
+ * @method string getOwnerId()
+ */
+class ModifyBackupStrategyRequest extends \RpcAcsRequest
+{
+
+    /**
+     * @var string
+     */
+    protected $method = 'POST';
+
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            'Dbs',
+            '2019-03-06',
+            'ModifyBackupStrategy',
+            'cbs'
+        );
+    }
+
+    /**
+     * @param string $backupPeriod
+     *
+     * @return $this
+     */
+    public function setBackupPeriod($backupPeriod)
+    {
+        $this->requestParameters['BackupPeriod'] = $backupPeriod;
+        $this->queryParameters['BackupPeriod'] = $backupPeriod;
+
+        return $this;
+    }
+
+    /**
+     * @param string $backupStartTime
+     *
+     * @return $this
+     */
+    public function setBackupStartTime($backupStartTime)
+    {
+        $this->requestParameters['BackupStartTime'] = $backupStartTime;
+        $this->queryParameters['BackupStartTime'] = $backupStartTime;
+
+        return $this;
+    }
+
+    /**
+     * @param string $clientToken
+     *
+     * @return $this
+     */
+    public function setClientToken($clientToken)
+    {
+        $this->requestParameters['ClientToken'] = $clientToken;
+        $this->queryParameters['ClientToken'] = $clientToken;
+
+        return $this;
+    }
+
+    /**
+     * @param string $backupPlanId
+     *
+     * @return $this
+     */
+    public function setBackupPlanId($backupPlanId)
+    {
+        $this->requestParameters['BackupPlanId'] = $backupPlanId;
+        $this->queryParameters['BackupPlanId'] = $backupPlanId;
+
+        return $this;
+    }
+
+    /**
+     * @param string $ownerId
+     *
+     * @return $this
+     */
+    public function setOwnerId($ownerId)
+    {
+        $this->requestParameters['OwnerId'] = $ownerId;
+        $this->queryParameters['OwnerId'] = $ownerId;
+
+        return $this;
+    }
+}
