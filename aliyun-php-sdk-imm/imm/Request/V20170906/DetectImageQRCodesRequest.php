35b9a113f717a0f6a198804bf9a2deeec035a46b
sdk-team <sdk-team@alibabacloud.com>
2019-10-22 10:07:21 +0800

    Supported ExternalId IMM.

---
+++ aliyun-php-sdk-imm/imm/Request/V20170906/DetectImageQRCodesRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace imm\Request\V20170906;
+
+/**
+ * @deprecated Please use https://github.com/aliyun/openapi-sdk-php
+ *
+ * Request of DetectImageQRCodes
+ *
+ * @method string getProject()
+ * @method string getImageUri()
+ */
+class DetectImageQRCodesRequest extends \RpcAcsRequest
+{
+
+    /**
+     * @var string
+     */
+    protected $method = 'POST';
+
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            'imm',
+            '2017-09-06',
+            'DetectImageQRCodes',
+            'imm'
+        );
+    }
+
+    /**
+     * @param string $project
+     *
+     * @return $this
+     */
+    public function setProject($project)
+    {
+        $this->requestParameters['Project'] = $project;
+        $this->queryParameters['Project'] = $project;
+
+        return $this;
+    }
+
+    /**
+     * @param string $imageUri
+     *
+     * @return $this
+     */
+    public function setImageUri($imageUri)
+    {
+        $this->requestParameters['ImageUri'] = $imageUri;
+        $this->queryParameters['ImageUri'] = $imageUri;
+
+        return $this;
+    }
+}
