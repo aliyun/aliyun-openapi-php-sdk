3ac67cb35848e56651b4a7aa2a4a11d646f86482
sdk-team <sdk-team@alibabacloud.com>
2019-11-28 16:26:16 +0800

    Supported API GetTxtRecordForVerify.

---
+++ aliyun-php-sdk-alidns/Alidns/Request/V20150109/GetTxtRecordForVerifyRequest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Alidns\Request\V20150109;
+
+/**
+ * @deprecated Please use https://github.com/aliyun/openapi-sdk-php
+ *
+ * Request of GetTxtRecordForVerify
+ *
+ * @method string getDomainName()
+ * @method string getType()
+ * @method string getUserClientIp()
+ * @method string getLang()
+ */
+class GetTxtRecordForVerifyRequest extends \RpcAcsRequest
+{
+
+    /**
+     * @var string
+     */
+    protected $method = 'POST';
+
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            'Alidns',
+            '2015-01-09',
+            'GetTxtRecordForVerify',
+            'alidns'
+        );
+    }
+
+    /**
+     * @param string $domainName
+     *
+     * @return $this
+     */
+    public function setDomainName($domainName)
+    {
+        $this->requestParameters['DomainName'] = $domainName;
+        $this->queryParameters['DomainName'] = $domainName;
+
+        return $this;
+    }
+
+    /**
+     * @param string $type
+     *
+     * @return $this
+     */
+    public function setType($type)
+    {
+        $this->requestParameters['Type'] = $type;
+        $this->queryParameters['Type'] = $type;
+
+        return $this;
+    }
+
+    /**
+     * @param string $userClientIp
+     *
+     * @return $this
+     */
+    public function setUserClientIp($userClientIp)
+    {
+        $this->requestParameters['UserClientIp'] = $userClientIp;
+        $this->queryParameters['UserClientIp'] = $userClientIp;
+
+        return $this;
+    }
+
+    /**
+     * @param string $lang
+     *
+     * @return $this
+     */
+    public function setLang($lang)
+    {
+        $this->requestParameters['Lang'] = $lang;
+        $this->queryParameters['Lang'] = $lang;
+
+        return $this;
+    }
+}
