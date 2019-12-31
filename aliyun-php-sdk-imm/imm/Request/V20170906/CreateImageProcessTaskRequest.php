25305c7d1bd403f3ef3d1e6c5f1ece7086aee5d0
sdk-team <sdk-team@alibabacloud.com>
2019-12-31 13:56:09 +0800

    Supported image for process.

---
+++ aliyun-php-sdk-imm/imm/Request/V20170906/CreateImageProcessTaskRequest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace imm\Request\V20170906;
+
+/**
+ * @deprecated Please use https://github.com/aliyun/openapi-sdk-php
+ *
+ * Request of CreateImageProcessTask
+ *
+ * @method string getProject()
+ * @method string getNotifyEndpoint()
+ * @method string getNotifyTopicName()
+ * @method string getTargetList()
+ * @method string getImageUri()
+ */
+class CreateImageProcessTaskRequest extends \RpcAcsRequest
+{
+
+    /**
+     * @var string
+     */
+    protected $method = 'POST';
+
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            'imm',
+            '2017-09-06',
+            'CreateImageProcessTask',
+            'imm'
+        );
+    }
+
+    /**
+     * @param string $project
+     *
+     * @return $this
+     */
+    public function setProject($project)
+    {
+        $this->requestParameters['Project'] = $project;
+        $this->queryParameters['Project'] = $project;
+
+        return $this;
+    }
+
+    /**
+     * @param string $notifyEndpoint
+     *
+     * @return $this
+     */
+    public function setNotifyEndpoint($notifyEndpoint)
+    {
+        $this->requestParameters['NotifyEndpoint'] = $notifyEndpoint;
+        $this->queryParameters['NotifyEndpoint'] = $notifyEndpoint;
+
+        return $this;
+    }
+
+    /**
+     * @param string $notifyTopicName
+     *
+     * @return $this
+     */
+    public function setNotifyTopicName($notifyTopicName)
+    {
+        $this->requestParameters['NotifyTopicName'] = $notifyTopicName;
+        $this->queryParameters['NotifyTopicName'] = $notifyTopicName;
+
+        return $this;
+    }
+
+    /**
+     * @param string $targetList
+     *
+     * @return $this
+     */
+    public function setTargetList($targetList)
+    {
+        $this->requestParameters['TargetList'] = $targetList;
+        $this->queryParameters['TargetList'] = $targetList;
+
+        return $this;
+    }
+
+    /**
+     * @param string $imageUri
+     *
+     * @return $this
+     */
+    public function setImageUri($imageUri)
+    {
+        $this->requestParameters['ImageUri'] = $imageUri;
+        $this->queryParameters['ImageUri'] = $imageUri;
+
+        return $this;
+    }
+}
---
+++ aliyun-php-sdk-imm/imm/Request/V20170906/GetOfficePreviewURLRequest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace imm\Request\V20170906;
+
+/**
+ * @deprecated Please use https://github.com/aliyun/openapi-sdk-php
+ *
+ * Request of GetOfficePreviewURL
+ *
+ * @method string getSrcType()
+ * @method string getExpires()
+ * @method string getProject()
+ * @method string getUseHTTPS()
+ * @method string getSrcUri()
+ */
+class GetOfficePreviewURLRequest extends \RpcAcsRequest
+{
+
+    /**
+     * @var string
+     */
+    protected $method = 'POST';
+
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            'imm',
+            '2017-09-06',
+            'GetOfficePreviewURL',
+            'imm'
+        );
+    }
+
+    /**
+     * @param string $srcType
+     *
+     * @return $this
+     */
+    public function setSrcType($srcType)
+    {
+        $this->requestParameters['SrcType'] = $srcType;
+        $this->queryParameters['SrcType'] = $srcType;
+
+        return $this;
+    }
+
+    /**
+     * @param string $expires
+     *
+     * @return $this
+     */
+    public function setExpires($expires)
+    {
+        $this->requestParameters['Expires'] = $expires;
+        $this->queryParameters['Expires'] = $expires;
+
+        return $this;
+    }
+
+    /**
+     * @param string $project
+     *
+     * @return $this
+     */
+    public function setProject($project)
+    {
+        $this->requestParameters['Project'] = $project;
+        $this->queryParameters['Project'] = $project;
+
+        return $this;
+    }
+
+    /**
+     * @param string $useHTTPS
+     *
+     * @return $this
+     */
+    public function setUseHTTPS($useHTTPS)
+    {
+        $this->requestParameters['UseHTTPS'] = $useHTTPS;
+        $this->queryParameters['UseHTTPS'] = $useHTTPS;
+
+        return $this;
+    }
+
+    /**
+     * @param string $srcUri
+     *
+     * @return $this
+     */
+    public function setSrcUri($srcUri)
+    {
+        $this->requestParameters['SrcUri'] = $srcUri;
+        $this->queryParameters['SrcUri'] = $srcUri;
+
+        return $this;
+    }
+}
