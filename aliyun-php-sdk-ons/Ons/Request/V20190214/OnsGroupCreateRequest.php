dda7be85f173c1631053ecaa1ee93c15a9c6f593
sdk-team <sdk-team@alibabacloud.com>
2019-12-31 14:36:25 +0800

    Add request parameter groupType for OnsGroupCreate.

--- aliyun-php-sdk-ons/Ons/Request/V20190214/OnsGroupCreateRequest.php
+++ aliyun-php-sdk-ons/Ons/Request/V20190214/OnsGroupCreateRequest.php
@@ -10,6 +10,7 @@ namespace Ons\Request\V20190214;
  * @method string getGroupId()
  * @method string getRemark()
  * @method string getInstanceId()
+ * @method string getGroupType()
  */
 class OnsGroupCreateRequest extends \RpcAcsRequest
 {
@@ -67,6 +68,19 @@ class OnsGroupCreateRequest extends \RpcAcsRequest
     {
         $this->requestParameters['InstanceId'] = $instanceId;
         $this->queryParameters['InstanceId'] = $instanceId;
+
+        return $this;
+    }
+
+    /**
+     * @param string $groupType
+     *
+     * @return $this
+     */
+    public function setGroupType($groupType)
+    {
+        $this->requestParameters['GroupType'] = $groupType;
+        $this->queryParameters['GroupType'] = $groupType;
 
         return $this;
     }
